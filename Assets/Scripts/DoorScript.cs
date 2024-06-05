14174b6acf7d645ef1d01295b751b72c720955a4
RookieDi <mvasy03@yahoo.ro>
2024-06-05 08:09:48 -0700

    room

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -1,18 +1,58 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorScript : MonoBehaviour
+public class Door : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject doorObject; // Reference to the door object
+    private Quaternion closedRotation; // The rotation when the door is closed
+    private Quaternion openedRotation; // The rotation when the door is opened
+    private bool isOpen = false; // Flag to track if the door is open
+
     void Start()
     {
-        
+        // Store the initial rotation as closed rotation
+        closedRotation = doorObject.transform.rotation;
+
+        // Calculate the opened rotation (e.g., rotate around the y-axis by 90 degrees)
+        openedRotation = Quaternion.Euler(0, 90, 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter(Collider other)
     {
-        
+        // Check if the colliding object has the "Move" script attached
+        Move moveScript = other.GetComponent<Move>();
+        if (moveScript != null)
+        {
+            OpenDoor();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        // Check if the colliding object has the "Move" script attached
+        Move moveScript = other.GetComponent<Move>();
+        if (moveScript != null)
+        {
+            CloseDoor();
+        }
+    }
+
+    void OpenDoor()
+    {
+        if (!isOpen)
+        {
+            
+            doorObject.transform.rotation = openedRotation;
+            isOpen = true;
+        }
+    }
+
+    void CloseDoor()
+    {
+        if (isOpen)
+        {
+            // Rotate the door back to the closed position
+            doorObject.transform.rotation = closedRotation;
+            isOpen = false;
+        }
     }
 }
--- Assets/Scripts/FloorManualGenerator.cs
+++ Assets/Scripts/FloorManualGenerator.cs
@@ -1,20 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class FloorManualGenerator : MonoBehaviour
+public class RoomGenerator : MonoBehaviour
 {
-  [SerializeField] private GameObject blackCube, whiteCube, greenCube;
-  [SerializeField] private GameObject romOne, romTwo, romThree;
-  private float distanceBetweenTiles = 1f;
+    public int gridSize = 5; // Size of the grid (number of cubes along each axis)
+    public GameObject wallPrefab; // Prefab for the walls
+    public GameObject blackCubePrefab; // Prefab for the black cube
+    public GameObject whiteCubePrefab; // Prefab for the white cube
 
-[ContextMenu("GenerateCubesRoom1")]
-  private void GenerateCubesRoom1()
-  {
-    for (int i = 0; i < 10; i++)
+    [ContextMenu("Generate Room")]
+    void GenerateRoom()
     {
-      Vector3 newPosition = new Vector3(0, 0, i * distanceBetweenTiles);
-      Instantiate(whiteCube,newPosition,Quaternion.identity,romOne.transform);
+      
+        // Generate floor
+        GenerateFloor();
+
+        // Generate walls
+        GenerateWalls();
     }
-  }
+
+    void GenerateFloor()
+    {
+        float cubeSpacing = 1f; // Spacing between cubes
+        Vector3 floorStartPosition = transform.position - new Vector3(gridSize * cubeSpacing / 2f, 0f, gridSize * cubeSpacing / 2f);
+
+        for (int x = 0; x < gridSize; x++)
+        {
+            for (int z = 0; z < gridSize; z++)
+            {
+                GameObject cubePrefab = (x + z) % 2 == 0 ? whiteCubePrefab : blackCubePrefab;
+                Vector3 cubePosition = floorStartPosition + new Vector3(x * cubeSpacing, 0f, z * cubeSpacing);
+                Instantiate(cubePrefab, cubePosition, Quaternion.identity);
+            }
+        }
+    }
+
+    void GenerateWalls()
+    {
+        float cubeSpacing = 1f; 
+        Vector3 wallStartPosition = transform.position - new Vector3(gridSize * cubeSpacing / 2f, 0f, gridSize * cubeSpacing / 2f);
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int x = 0; x < gridSize; x++)
+            {
+                for (int z = 0; z < gridSize; z++)
+                {
+                    Vector3 cubePosition = wallStartPosition + new Vector3(x * cubeSpacing, i * cubeSpacing, z * cubeSpacing);
+
+                    if (i == 0)
+                    {
+                        GameObject cube = Instantiate(blackCubePrefab, cubePosition, Quaternion.identity);
+                    }
+                    else
+                    {
+                        if (x == 0 || x == gridSize - 1 || z == 0 || z == gridSize - 1)
+                        {
+                            GameObject cube = Instantiate(blackCubePrefab, cubePosition, Quaternion.identity);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+   
 }
